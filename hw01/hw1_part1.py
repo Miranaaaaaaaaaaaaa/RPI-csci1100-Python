c8e5fc0deb2a0b34737c7d3eccb3fb63c7bcc7be
Miranaaaaaaaaaaaaa <37964477+Miranaaaaaaaaaaaaa@users.noreply.github.com>
2018-04-10 17:27:28 -0400

    second modified

--- hw01/hw1_part1.py
+++ hw01/hw1_part1.py
@@ -12,8 +12,10 @@ print(d_moon)
 d_moon = float(d_moon)
 rate_moon = input("Enter the rate the Moon is moving away (in/year) -> ")
 print(rate_moon)
+
 rate_moon = float(rate_moon)
 distance = float(d_sun*(r_moon/r_sun))
 recede = float(distance-d_moon)
 rate = float((recede*5280*12)/rate_moon)
+
 print('The Moon will have exactly the same apparent size as the Sun when it is {0:.2f} miles away.\nThe Moon will need to recede another {1:.2f} miles\nWhich will occur in {2:.0f} years at the current rate.'.format(distance,recede,rate))
